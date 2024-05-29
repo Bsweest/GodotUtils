2c6070dbe5f6d24f6f22a9b79461c432a0751d6b
TeraBoxDev <phanhuymanh1409@gmail.com>
2024-05-29 14:56:14 +0700

    fix bug: generator is not shipped with package

--- GodotUtils.InstanceResolver.UnitTests/Program.cs
+++ GodotUtils.InstanceResolver.UnitTests/Program.cs
@@ -1,8 +1,1 @@
-﻿using Godot;
-using GodotUtils.InstanceResolver;
-using GodotUtils.InstanceResolver.UnitTests;
-
-var p = new PackedScene();
-
-var x = p.Init<Parameter_Test>(item => item.Map(new() { Test = "Hello World" }));
-var y = p.Init<Test>();
+﻿
